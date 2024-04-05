a4463072f5648e136d48dc47d2708570031b607c
umesh <umesh.ladumor@eximietas.design>
2024-04-05 15:35:42 +0530

    find the sum of even number

---
+++ Umesh-l/find_sum_of_first_n_even_num.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char **argv)
+{
+	if(argc!=2)
+	{
+		printf("Useg:./a.out num \n");
+		return 0;
+	}
+	int i,sum=0;
+	for(i=1;i<=atoi(argv[1]);i++)
+	{
+		if(i%2==0)
+		{
+			sum=sum+i;
+		}
+	}
+	printf("sum of number = %d\n",sum);
+}
+	
