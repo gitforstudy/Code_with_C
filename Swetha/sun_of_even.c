f3ce7999ee515a358bba5895249f60ba1a96cac2
swetha <swetha.madarapu@eximietas.design>
2024-04-07 11:15:26 +0530

    sumofnum

--- Swetha/sun_of_even.c
+++ Swetha/sun_of_even.c
@@ -4,12 +4,10 @@ int main()
 	int num,i,sum;
 	printf("enter a number:");
 	scanf("%d",&num);	        
-	for(i=1;i<=num;i++)
-	{
-	if(i%2==0)
-	{
-	      sum=sum+i;
-	}
+	for(i=1;i<=num;i++) {
+		if(i%2==0) {
+			sum=sum+i;
+		}
 	}
 	printf("%d",num);
 }
