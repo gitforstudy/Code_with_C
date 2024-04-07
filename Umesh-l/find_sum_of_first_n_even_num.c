d08593eece167ab21eb0de6c457b9e54fee0cab3
umesh <umesh.ladumor@eximietas.design>
2024-04-07 15:11:56 +0530

    sum of even number

--- Umesh-l/find_sum_of_first_n_even_num.c
+++ Umesh-l/find_sum_of_first_n_even_num.c
@@ -2,17 +2,24 @@
 #include<stdlib.h>
 int main(int argc,char **argv)
 {
-	if(argc!=2)
+	if(argc!=3)
 	{
-		printf("Usage:./a.out number \n");
+		printf("Usage:./a.out number1 to number2 \n");
 		return 0;
 	}
-	int i,sum=0;
-	for(i=1;i<=atoi(argv[1]);i++)
+	int i,sum=0,num1;
+	num1=atoi(argv[1]);
+	if(num1%2)
+	{
+		num1=num1+1;
+	}
+	
+	for(i=num1;i<=atoi(argv[2]);i++)
 	{
 		if(i%2==0)
 		{
 			sum=sum+i;
+			
 		}
 	}
 	printf("sum of number = %d\n",sum);
