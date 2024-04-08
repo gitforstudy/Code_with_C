bb8d9cc856570e707ed86fe89fbf1d72a6def51c
umesh <umesh.ladumor@eximietas.design>
2024-04-08 17:24:56 +0530

    modified pattern_02

--- Umesh-l/pattern_02.c
+++ Umesh-l/pattern_02.c
@@ -3,16 +3,16 @@ void main()
 {
 	int i,j,k,l,num;
 	printf("enter the number\n");
-	
+	scanf("%d",&num);	
 
-	for(i=1;i<=5;i++)//first loop to print all rows
+	for(i=1;i<=num;i++)//first loop to print all rows
 	{
 		for(j=1;j<=i;j++)//loop for printing * character
 		{
-			if(j!=5)// 5 row not printing 
+			if(j!=num)// 5 row not printing 
 			printf("*");
 		}
-		for(k=9-2*i;k>=1;k--)//inner loop 1 to print spaces
+		for(k=((num-1)+num)-2*i;k>=1;k--)//inner loop 1 to print spaces
 		{
 			printf(" ");
 		}
