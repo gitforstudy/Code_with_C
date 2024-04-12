f1253e26bd4912d923b74f90980307c75eec70e6
madhuri <madhuri.jinka@eximietas.design>
2024-04-12 11:01:02 +0530

    relational_operator

---
+++ Madhuri-j/relational_opr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int a = 10, b = 5, c = 2;
+	if(a > b) {
+		printf("a is greater than b");
+	} else if(b < c) {
+		printf("c is greater than b");
+	} else {
+		printf("b is greater");
+	}
+	return 0;
+}
+
+
+
