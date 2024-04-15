d20421d3dbbf2a0e0a391d7e279d4f3d115faa9f
Renjitha-123 <165981074+Renjitha-123@users.noreply.github.com>
2024-04-15 11:54:49 +0530

    Update 2nd_large_ele_arr.c

--- Renjitha-o/2nd_large_ele_arr.c
+++ Renjitha-o/2nd_large_ele_arr.c
@@ -1,21 +1,23 @@
-//program to find 2nd largest element in an array
 
 #include <stdio.h>
 
+int second_lar_ele(int arr[],int size) {
+        int temp = 0;
+        for (int i = 0; i < size - 1; i++) {
+                for (int j = 1; j < size - 1; j++) {
+                        if (arr[i] < arr[j]) {// checking the array is sorted or not,if not swapping array elements using temp variable
+                                temp = arr[i];
+                                arr[i] = arr[j];
+                                arr[j] = temp;
+                        }
+                }
+        }
+        return arr[size - 2];
+}
 int main (void)
 {
-	int arr[6] = {6,8,2,3,9,1},temp = 0;
-	int size = sizeof(arr)/sizeof(arr[0]);// finding size of array
-	for (int i = 0; i < size - 1; i++) {
-		for (int j = 1; j < size - 1; j++) {
-			if (arr[i] < arr[j]) {// checking the array is sorted or not,if not swapping array elements using temp variable
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-		}
-	}
-
-	printf ("2nd largest element in this array is :%d",arr[size - 2]);// printing 2nd largest element
-	return 0;
+        int arr[6] = {6,8,2,3,9,1},temp = 0;
+        int size = sizeof(arr)/sizeof(arr[0]);// finding size of array
+        printf ("2nd largest element in this array is :%d",second_lar_ele(arr,size));// printing 2nd largest element
+        return 0;
 }
