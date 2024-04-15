85c194aec422e1f4c3d7baab2c9d3b43b1a02aa1
Madhuri-4 <165981323+Madhuri-4@users.noreply.github.com>
2024-04-15 15:45:24 +0530

    Update char_arr_reverse.c

--- Madhuri-j/char_arr_reverse.c
+++ Madhuri-j/char_arr_reverse.c
@@ -1,23 +1,23 @@
+//C program to reverse character array
 #include <stdio.h>
+void char_arr(int n, char arr[]);
 int main(void)
 {
-	char arr[10] = {'a','b','c','d','e','f','g','h','i','j'};
-	int n = 10;
-	/*printf("Enter the number of characters: ");
-	scanf("%d", &n);
-	printf("Enter the characters to an array: ");
-	for(int i = 0; i < n; i++) {
-		scanf("%c", &arr[i]);
-	}*/
-	printf("Original character array: ");
-	for(int i = 0; i < n; i++) {
-		printf("%c", arr[i]);
-	}
-	printf("\n");
-	printf("Reversed char array: ");
-	for(int i = n-1; i >= 0; i--) {
-		printf("%c", arr[i]);
-	}
-	printf("\n");
-	return 0;
+        char arr[10] = {'a','b','c','d','e','f','g','h','i','j'};
+        int n = 10;
+        printf("Original character array: ");//printing original array
+        for(int i = 0; i < n; i++) {
+                printf("%c", arr[i]);
+        }
+        printf("\n");
+        char_arr(n, arr);//function call
+        return 0;
+}
+
+void char_arr(int n, char arr[]) { //function definition
+        printf("Reversed char array: ");//printing reversed array
+        for(int i = n-1; i >= 0; i--) {
+                printf("%c", arr[i]);
+        }
+        printf("\n");
 }
