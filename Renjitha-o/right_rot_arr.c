75a002d9e942ff601309f91ce00d59e6443cc0f4
Renjitha-123 <165981074+Renjitha-123@users.noreply.github.com>
2024-04-15 11:45:49 +0530

    Update right_rot_arr.c

--- Renjitha-o/right_rot_arr.c
+++ Renjitha-o/right_rot_arr.c
@@ -1,10 +1,28 @@
 // program to rotate array in right position
 
 #include <stdio.h>
+
+void right_rot_arr(int arr[],int num,int size) {
+        int count = 0,temp;
+        while ( count < num) {// while loop for how may times you neeed to rotate
+                temp = arr[size - 1];
+                for (int j = size; j >= 0; j--){
+                        arr[j + 1] = arr[j];// store the  i+ 1 element in previous index
+                        //printf("%d\n",arr[j]);
+                }
+                count++;
+                arr[0] = temp;// add temp into last index
+        }
+        printf ("ARRAY AFTER RIGHT ROTATION\n");
+        for (int i = 0; i < size; i++) {
+                printf("arr[%d] :%d\t",i,arr[i]);
+        }
+        printf("\n");
+}
 int main(void)
 {
         int arr[5] = {1,2,3,4,5};
-        int i,j,k,temp,num;
+        int k,num;
         int size = sizeof(arr)/sizeof(arr[0]);
         printf ("ARRAY BEFORE RIGHT ROTATION\n");
         for (k = 0; k < size; k++) {
@@ -13,20 +31,6 @@ int main(void)
         printf("\n");
         printf("enter the times ,u need to rotate the array");
         scanf("%d",&num);
-        int count = 0;
-        while ( count < num) {// while loop for how may times you neeed to rotate
-                temp = arr[size - 1];
-                for (j = size; j >= 0; j--){
-                        arr[j + 1] = arr[j];// store the  i+ 1 element in previous index
-                        //printf("%d\n",arr[j]);
-                }
-                count++;
-                arr[0] = temp;// add temp into last index
-        }
-        printf ("ARRAY AFTER RIGHT ROTATION\n");
-        for (i = 0; i < size; i++) {
-                printf("arr[%d] :%d\t",i,arr[i]);
-        }
-        printf("\n");
+        right_rot_arr(arr,num,size);
         return 0;
 }
