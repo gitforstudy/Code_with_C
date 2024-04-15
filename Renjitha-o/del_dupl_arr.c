77ea9a134b7687e1ee7935068fdd3fbbac7e452a
Renjitha-123 <165981074+Renjitha-123@users.noreply.github.com>
2024-04-15 11:50:29 +0530

    Update del_dupl_arr.c

--- Renjitha-o/del_dupl_arr.c
+++ Renjitha-o/del_dupl_arr.c
@@ -1,28 +1,32 @@
 // program to delete duplicate elements in an array
-
 #include <stdio.h>
-void main(void)
-{
-	int arr[] = {4,5,6,2,1,1,3,5};
-	int size = sizeof(arr)/sizeof(arr[0]);
-	int barr[10],j = 0;
-	printf ("array before deleting duplicate elements");
-	for (int i = 0; i < size; i++) {
+
+void delete_dup(int arr[],size,int barr[]) {
+        for (int i = 0; i < size - 1;i++) {
+                if (arr[i] != arr[i + 1]) {// compare 1st elemnt with all other elements and if it is not equal.
+                        barr[j] = arr[i];//add it into another array
+                        j++;//increase index of 2nd array
+                }
+                //barr[j++] = arr[ size - 1];
+
+        }
+        for (int i = 0; i < j; i++) {// copy the new array into original array
+                arr[i] = barr[i];
+        }
+        printf ("%d\n",size);
+        for (int i = 0; i < j; i++) {// print array after removing duplicate elements
                 printf("arr[%d] :%d\n",i,arr[i]);
         }
-	for (int i = 0; i < size - 1;i++) {
-		if (arr[i] != arr[i + 1]) {
-			barr[j] = arr[i];
-			j++;
-		}
-		//barr[j++] = arr[ size - 1];
-				
-	}
-	for (int i = 0; i < j; i++) {
-		arr[i] = barr[i];
-	}
-	printf ("%d\n",size);
-	for (int i = 0; i < j; i++) {
-		printf("arr[%d] :%d\n",i,arr[i]);
-	}
+}
+void main(void)
+{
+        int arr[] = {4,5,6,2,1,1,3,5};
+        int size = sizeof(arr)/sizeof(arr[0]);
+        int barr[10],j = 0;
+        printf ("array before deleting duplicate elements");
+        for (int i = 0; i < size; i++) {// print array
+                printf("arr[%d] :%d\n",i,arr[i]);
+        }
+        printf("after deleting duplicate elements:\n");
+        delete_dup(arr,size,barr);
 }
