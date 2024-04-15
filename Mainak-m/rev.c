19b04d6c0662c1b38d65655f52c7cac22ae7aa17
Mainak-m <mainak.mitra@eximietas.design>
2024-04-15 11:40:58 +0530

    changed the rev program

--- Mainak-m/rev.c
+++ Mainak-m/rev.c
@@ -1,7 +1,13 @@
-// C Program to Reverse an Array by Printing it from The Last Element to the First Element
-
 #include <stdio.h>
 #define N 1000
+
+void reverseArray(int arr[], int size) {
+    printf("Reversed array: ");
+    for (int i = size - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
 int main() {
     int arr[N];
@@ -11,17 +17,14 @@ int main() {
     printf("Enter the size of the array: ");
     scanf("%d", &n);
 
-    // take the the array elements from the user
+    // take the array elements from the user
     printf("Enter an array: ");
-    for (int i = 0; i< n; i++){
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Printing the reverse of the array elements
-    printf("Reversed array: ");
-    for (int i = n-1; i>=0; i--){
-        printf("%d ", arr[i]);
-    }
+    // Call the function to reverse the array
+    reverseArray(arr, n);
 
     return 0;
 }
