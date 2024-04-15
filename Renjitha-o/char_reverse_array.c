4a642e8b4a8591500d3b80849795f2cf28723b04
Renjitha-123 <165981074+Renjitha-123@users.noreply.github.com>
2024-04-15 11:30:14 +0530

    Update char_reverse_array.c

--- Renjitha-o/char_reverse_array.c
+++ Renjitha-o/char_reverse_array.c
@@ -2,18 +2,22 @@
 
 #include <stdio.h>
 
+void char_reverse_array(char arr[],int size) {
+        int j = 0;
+        char barr[10];
+        for (int i = size - 1; i >= 0; i--) {// from last element storing the elements into another element
+                barr[j] = arr[i];
+                j++;
+        }
+        for (int i = 0; i < 10; i++) {
+                printf("arr[%d] : %c\n",i,barr[i]);
+        }
+}
 int main(void)
 {
-	char arr[10] = {'a','b','c','d','e','f','g','h','i','j'};
-	char barr[10];
-	int size = sizeof(arr)/sizeof(arr[0]);
-	int j = 0;
-	for (int i = size - 1; i >= 0; i--) {// from last element storing the elements into another element
-		barr[j] = arr[i];
-		j++;	
-	}
-	for (int i = 0; i < 10; i++) {
-		printf("arr[%d] : %c\n",i,barr[i]);
-	}
-	return 0;
+        char arr[10] = {'a','b','c','d','e','f','g','h','i','j'};
+        int size = sizeof(arr)/sizeof(arr[0]);
+        printf("array after reverse:");
+        char_reverse_array(arr,size);
+        return 0;
 }
