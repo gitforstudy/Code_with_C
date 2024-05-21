8eda9bd636007d389133d55e48a87b4b30b7bfa9
Madhuri-4 <madhuri.jinka@eximietas.design>
2024-05-21 10:12:36 +0530

    string word replace

---
+++ Madhuri-j/Practice_Programs/revbits.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void to_bin(int num) {
+	for(int i=31;i>=0;i--)
+    	printf("%d", num>>i&1);
+    	printf("\n");
+}
+
+int revbits(int num)
+{
+    	int i, j, x, y;
+    	for(i = 31, j = 0; i > j; i--, j++) {
+        	x=num>>i&1;
+        	y=num>>j&1;
+        	if(x != y) {
+            		num = num^1<<j;
+            		num = num^1<<i;
+        	}
+    	}
+    	printf("After Reverse: \n");
+    	to_bin(num);
+    	return num;
+}
+
+int main(void)
+{
+    	int num,i;
+	printf("Enter the number: ");
+	scanf("%d", &num);
+    	printf("Before Reverse: \n");
+    	to_bin(num);
+    	revbits(num);
+    	return 0;
+}
---
+++ Madhuri-j/Practice_Programs/swap_nibbles.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void to_bin(int number)
+{
+	int i;
+    	for(i = 15; i >= 0; i--) {
+        	printf("%d",number>>i&1);//printing the binary number for number
+    	}
+    	printf("\n");
+}
+
+void swap_nibble(int number)
+{
+    	int left, right;
+    	left = number & 0x0f0f;//masking 2nd and 4th nibbles
+    	left = left << 4;//left shifting
+    	right = number & 0xf0f0;//masking 1st and 3rd nibble
+    	right = right >> 4;//right shifting
+    	number = left | right;//or operation for left & right
+    	to_bin(number);//func call to convert number to binary
+    
+}
+
+
+int main(void)
+{
+    	int number;
+    	printf("Enter the number: ");
+    	scanf("%x", &number);
+    	to_bin(number);//func call to convert number to binary
+    	swap_nibble(number);//func call to swap nibbles
+    	return 0;
+    
+}
