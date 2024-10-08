70f19a875e1b50553b34238a5ff12aab2befd6c9
Madhuri-4 <165981323+Madhuri-4@users.noreply.github.com>
2024-10-08 09:38:22 +0530

    del_duplicate.c

--- Madhuri-j/del_duplicate.c
+++ Madhuri-j/del_duplicate.c
@@ -30,3 +30,26 @@ void del_duplicate(int n, int a[]) {
 		printf("%d", a[i]);
 	}
 }
+
+
+
+/*
+int removeDuplicates(int* nums, int numsSize) {
+    if(numsSize == 0) {
+    return 0;
+  }
+
+  int ans = 1;
+  for(int i = 1; i<numsSize; i++){
+    for(int j=i-1;j<i;j++){
+      if(nums[i] == nums[j]){
+        continue;
+      }else{
+        nums[ans] = nums[i];
+        ans++;
+      }
+    }
+  }
+  return ans;
+}
+*/
